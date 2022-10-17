2808cc3e872ed38489f49328a64a54faf3f169ee
Kevin Price <collegekevin@gmail.com>
2022-10-17 10:17:29 -0700

    commit tree f3a3134bec489e01f02bc94d09b4c8294a4ec1a8

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch ("https://anapioficeandfire.com/api/books")
+    //console.log(fetch ("https://anapioficeandfire.com/api/books"))
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json))
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerText = book.name;
     main.appendChild(h2);
   });
 }
